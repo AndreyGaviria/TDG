35d4bc914c77cebaf522694bb357ca1ad1863476
AndreyGaviria <andrey.gaviria454@pascualbravo.edu.co>
2026-09-16 22:20:38 -0500

    Actualización1

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div className="container">
-            <h1>Welcome to Small Business Solutions :D</h1>
+            <h1>HOLA MUNDO</h1>
             <a href="/main">Iniciar</a>
         </div>
     )
